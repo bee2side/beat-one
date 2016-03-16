4fe56486e95da4a5b592b4c836dae2208659c9da
bee2side <bee2side@gmail.com>
2016-03-16 21:24:27 +0900

    1

--- js/pageControl.js
+++ js/pageControl.js
@@ -4,10 +4,12 @@ $(document).ready(function(){
 		if(menuNum == 0){
 			$(".menu_open").css("display", "none");
 			$(".menu_normal").css("display", "block");
+			$("body").unbind('touchmove');
 			menuNum = 1;
 		}else{
 			$(".menu_open").css("display", "block");
 			$(".menu_normal").css("display", "none");
+			$("body").bind('touchmove', function(e){e.preventDefault()});
 			menuNum = 0;
 		}
 	};
