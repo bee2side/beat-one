5e34aa650ce942040a99b58b5380d670ab3b1e6c
bee2side <bee2side@gmail.com>
2016-03-16 22:13:43 +0900

    1

--- footer.php
+++ footer.php
@@ -1,7 +1,7 @@
 			<div id="footer">
 				<span class="twitterbtn"></span>
 				<span class="facebookbtn"></span>
-				<span class="copyright">P&nbsp;+82&nbsp;10&nbsp;9880&nbsp;1841<br>bee2side@gmail.com<br>&#169;<?php bloginfo('name'); ?></span>
+				<span class="copyright">M.&nbsp;+82&nbsp;10&nbsp;9880&nbsp;1841<br>I.&nbsp;bee2side@gmail.com<br>&#169;<?php bloginfo('name'); ?></span>
 				<a href="<?php echo home_url(); ?>"><span class="b2btn"></span></a>
 			</div>
 		</div>
