26ad25b11605af7aa49db1ce90431d208d899d76
bee2side <bee2side@gmail.com>
2016-03-29 22:57:18 +0900

    1

--- js/pageControl.js
+++ js/pageControl.js
@@ -37,10 +37,10 @@ $(document).ready(function(){
 	function topImageSize(winWidth){
 		if(winWidth>1640){
 			$(".top_img_container").css("background-size", winWidth+"px" );
-		}else if(winWidth>1048){
+		}else if(winWidth>1064){
 			$(".top_img_container").css("background-size", "auto" );
 		}else{
-			$(".top_img_container").css("background-size", "820px, 100px" );
+			$(".top_img_container").css("background-size", "1000px, 100px" );
 		}
 	};
 });
