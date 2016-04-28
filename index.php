abff6dd694355b188595e18c3fb4b428a5f151e2
bee2side <bee2side@gmail.com>
2016-04-28 17:08:49 +0900

    1

--- index.php
+++ index.php
@@ -12,7 +12,7 @@
 					<?php the_title(); ?>
 				</a>
 				<span class="index_article_info">
-					<?php the_category('&nbsp;&nbsp;&nbsp;∙&nbsp;&nbsp;&nbsp;'); ?>
+					<?php the_category('&nbsp;&nbsp;&nbsp;&nbsp;∙&nbsp;&nbsp;&nbsp;&nbsp;'); ?>
 				</span>
 			</article>
 		<?php endwhile; else: ?>
